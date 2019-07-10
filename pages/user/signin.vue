b6eba3ebc5632f663233c869f62cfb261be4c22d
moon-hai <moon.hai@1pac.vn>
2019-07-10 15:09:26 +0700

    add link for user pages

--- pages/user/signin.vue
+++ pages/user/signin.vue
@@ -4,9 +4,6 @@
       <div class="row">
         <div class="col-md-6 offset-md-3 col-xs-12">
           <h1 class="text-xs-center">Sign up</h1>
-          <p class="text-xs-center">
-            <a href="">Have an account?</a>
-          </p>
 
           <ul class="error-messages">
             <li>That email is already taken</li>
@@ -23,9 +20,13 @@
               <input class="form-control form-control-lg" type="password" placeholder="Password">
             </fieldset>
             <button class="btn btn-lg btn-primary pull-xs-right">
-              Sign up
+              Sign in
             </button>
           </form>
+
+          <p class="text-xs-center">
+            <nuxt-link to="/user/signup">Have an account?</nuxt-link>
+          </p>
         </div>
       </div>
     </div>
