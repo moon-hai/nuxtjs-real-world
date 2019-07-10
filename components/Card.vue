b03c921b9d46c1689675bddbdeaf00c1b231bce8
moon-hai <moon.hai@1pac.vn>
2019-07-10 15:53:00 +0700

    tmp seperate components

---
+++ components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">
+    <div class="card-block">
+      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    </div>
+    <div class="card-footer">
+      <nuxt-link to="/user/1/profile" class="comment-author"><img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img" /></nuxt-link>
+      &nbsp;
+      <nuxt-link to="/user/1/profile" class="comment-author">Jacob Schmidt</nuxt-link>
+      <span class="date-posted">Dec 29th</span>
+      <span class="mod-options">
+        <i class="ion-edit"></i>
+        <i class="ion-trash-a"></i>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'card'
+}
+</script>
